39507fe87e20d2abe8d6ee3c7094b472b38e4861
mekanican <boommerang73@gmail.com>
2023-10-24 12:25:09 +0700

    Update GameState with Double-Buffer and mutex lock on read/write

--- Assets/Scripts/GameState/GameState.cs
+++ Assets/Scripts/GameState/GameState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Server;
 using UnityEngine;
 
@@ -7,27 +8,62 @@ public class GameState
 {
     private ConnectionHandler connHandler;
     private PlayerInfo currentPlayer;
-    private PlayerInfo otherPlayer;
+    // Double-Buffer Pattern
+    private PlayerInfo[] otherPlayer; // One for current 1 for buffering
     private bool isEnd;
     private bool isWin;
+
+    private static Mutex _readMut = new Mutex();
+    private static Mutex _writeMut = new Mutex();
 
     public GameState(ConnectionHandler connHandler, PlayerInfo currentPlayer, PlayerInfo otherPlayer, bool isEnd = false, bool isWin = false)
     {
         this.connHandler = connHandler;
         this.currentPlayer = currentPlayer;
-        this.otherPlayer = otherPlayer;
+        this.otherPlayer = new[]
+        {
+            otherPlayer,
+            otherPlayer
+        };
         this.isEnd = isEnd;
         this.isWin = isWin;
     }
 
     public void SendCurrentData()
     {
-        connHandler.SendBytes(currentPlayer.Serialize());
+        var t = new Thread(() =>
+        {
+            _readMut.WaitOne();
+            connHandler.SendBytes(currentPlayer.Serialize());
+            _readMut.ReleaseMutex();
+        });
+        t.Start();
     }
 
     public void RecvOtherData()
     {
-        otherPlayer = PlayerInfo.Deserialize(connHandler.RecvBytes());
+        var t = new Thread(() =>
+        {
+            _writeMut.WaitOne();
+            otherPlayer[1] = PlayerInfo.Deserialize(connHandler.RecvBytes());
+            SwapBuffer();
+            _writeMut.ReleaseMutex();
+        });
+        t.Start();
     }
 
+    private void SwapBuffer()
+    {
+        (otherPlayer[0], otherPlayer[1]) = (otherPlayer[1], otherPlayer[0]);
+    }
+
+    public Vector2 GetOtherPosition()
+    {
+        return otherPlayer[0].Position;
+    }
+
+    public void SetCurrentPosition(Vector2 position)
+    {
+        currentPlayer.Position = position;
+    }
 }
