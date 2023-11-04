c3bb36fbbb9e9b5701b4087d41280a34d341ca17
phphuc612 <phphuc612@gmail.com>
2023-11-04 20:42:57 +0700

    add loader

---
+++ Assets/Scripts/Scene/ASyncLoad.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ASyncLoad : MonoBehaviour
+{
+    public GameObject loadingScene;
+    public Slider loadingSlider;
+
+    public void loadScene(int sceneId)
+    {
+        StartCoroutine(loadLevelAsync(sceneId));
+    }
+
+    private IEnumerator loadLevelAsync(int sceneId)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneId);
+
+        loadingScene.SetActive(true);
+
+        double i = 0;
+        while (!loadOperation.isDone)
+        {
+            float progress = Mathf.Clamp01(loadOperation.progress / .9f);
+            
+            Debug.Log("Loading progress: " + progress);
+            loadingSlider.value = progress;
+            yield return null;
+        }
+    }
+}
