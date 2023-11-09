fb50372cba7fd06e6d85eacd5f47f1cbb7b724da
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-09 14:30:47 +0700

    Implement animation player

--- Assets/Scripts/Sample_Player.cs
+++ Assets/Scripts/Sample_Player.cs
@@ -23,12 +23,16 @@ public class Player : MonoBehaviour
     private const float GAUGE_PER_TICK = 0.01f;
 
     private Vector2 inverseX;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        jumpVec = new Vector2(Mathf.Cos(jumpAngle * Mathf.Deg2Rad), Mathf.Sin(jumpAngle * Mathf.Deg2Rad)); // 45 dec
+        jumpVec = new Vector2(
+            Mathf.Cos(jumpAngle * Mathf.Deg2Rad),
+            Mathf.Sin(jumpAngle * Mathf.Deg2Rad)
+        ); // 45 dec
         maxVel = Vector2.right * speed;
         currVel = Vector2.zero;
         isFlip = false;
@@ -50,45 +54,74 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    // TODO: Update the crouch and hit_wall animationß
     void Update()
     {
-        if (isTouchGround)
+        // rb.velocity.y = 0 happens in 2 cases:
+        // case 1: when the player is on the ground
+        // case 2: when the player hits the highest point of the jump/hit the
+        // ceiling, the velocity will be 0, then the player will fall down
+        if (rb.velocity.y == 0f)
         {
-            var keyboardHorizontal = Input.GetAxisRaw("Horizontal");
-            if (keyboardHorizontal < 0)
+            // Check if player is on the ground
+            if (isTouchGround)
             {
-                isFlip = true;
-            }
-            else if (keyboardHorizontal > 0)
-            {
-                isFlip = false;
-            }
-            transform.localScale = (isFlip ? lookRight * inverseX : lookRight);
-            // Charging the jump
-            if (Input.GetButton("Jump"))
-            {
-                currVel = Vector2.zero;
-                isFill = true;
-                return;
-            }
-            if (Input.GetButtonUp("Jump"))
-            {
-                isFill = false;
-                // Jump straight up
-                if (keyboardHorizontal == 0)
+                // Play "idle" animation
+                animator.Play("idle");
+                // Check if player is moving
+                var keyboardHorizontal = Input.GetAxisRaw("Horizontal");
+                if (keyboardHorizontal < 0)
                 {
-                    rb.AddForce(jumpGauge * jumpForce * Vector2.up, ForceMode2D.Impulse);
+                    isFlip = true;
+                    animator.Play("move");
                 }
-                else // Hold left | right & jump
+                else if (keyboardHorizontal > 0)
                 {
-                    rb.AddForce(jumpGauge * jumpForce * (isFlip ? jumpVec * inverseX : jumpVec), ForceMode2D.Impulse);
+                    isFlip = false;
+                    animator.Play("move");
                 }
+                transform.localScale = isFlip ? lookRight * inverseX : lookRight;
+                // Charging the jump
+                if (Input.GetButton("Jump"))
+                {
+                    currVel = Vector2.zero;
+                    isFill = true;
+                    return;
+                }
+                // Jump
+                if (Input.GetButtonUp("Jump"))
+                {
+                    isFill = false;
 
-                jumpGauge = 0;
-                isTouchGround = false;
-                return;
+                    // Jump straight up
+                    if (keyboardHorizontal == 0)
+                    {
+                        rb.AddForce(jumpGauge * jumpForce * Vector2.up, ForceMode2D.Impulse);
+                    }
+                    else // Hold left | right & jump
+                    {
+                        rb.AddForce(
+                            jumpGauge * jumpForce * (isFlip ? jumpVec * inverseX : jumpVec),
+                            ForceMode2D.Impulse
+                        );
+                    }
+
+                    jumpGauge = 0;
+                    isTouchGround = false;
+                    return;
+                }
+                currVel = maxVel * keyboardHorizontal;
             }
-            currVel = maxVel * keyboardHorizontal;
+        }
+        // If the rb.velocity.y > 0, the player is jumping
+        else if (rb.velocity.y > 0f)
+        {
+            animator.Play("jump");
+        }
+        // Else, it means that the player is falling down
+        else
+        {
+            animator.Play("down_air");
         }
     }
 
