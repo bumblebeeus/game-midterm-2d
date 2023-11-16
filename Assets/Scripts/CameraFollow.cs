d1f412b3bef2e79bd6c775407201cef7e6e12faa
Nguyen Van Loc <vanloc1808@gmail.com>
2023-11-16 21:05:51 +0700

    Attach character skin 2

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private GameObject player;
+
     void Start()
     {
         player = GameObject.Find("Character");
+        if (player == null)
+        {
+            player = GameObject.Find("Character2");
+        }
     }
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, Mathf.Lerp
-            (transform.position.y, player.gameObject.transform.position.y + 3, 0.01f), -10);
+        transform.position = new Vector3(
+            transform.position.x,
+            Mathf.Lerp(transform.position.y, player.gameObject.transform.position.y + 3, 0.01f),
+            -10
+        );
     }
 }
