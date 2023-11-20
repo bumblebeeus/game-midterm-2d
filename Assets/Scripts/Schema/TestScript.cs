568309f60f3a51700633dc7dea9e3555fe4446fc
phphuc612 <phphuc612@gmail.com>
2023-11-20 21:51:42 +0700

    update

--- Assets/Scripts/Schema/TestScript.cs
+++ Assets/Scripts/Schema/TestScript.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using DataBase;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class TestScript: MonoBehaviour
 {
     void Start()
     {
-        DataBase.Player player = new DataBase.Player();
+        StartCoroutine(test());
+    }
 
+    public IEnumerator test() {
+        DatabaseManager db = DatabaseManager.getInstance();
+        using (UnityWebRequest www = db.createWebRequest("db_connection.php", "GET", "{}")) {
+            yield return www.SendWebRequest();
+            Debug.Log(www.downloadHandler.text);
+        }
     }
 
     void Update()
